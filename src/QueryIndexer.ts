534ab8517ef8184ea99360d62437e52ecd72ba62
Dmytro Tolok <tolok.d@gmail.com>
2023-06-25 23:29:31 +0200

    feat(QueryIndexer): add maxTotalItems field, reverse order of next

--- src/QueryIndexer.ts
+++ src/QueryIndexer.ts
@@ -1,20 +1,23 @@
+export type QueryMeta = {
+  offset: number;
+  limit: number;
+  chunkN: number;
+};
+
 export default class QueryIndexer {
   items: QueryMeta[] = [];
+  maxTotalItems: number;
   constructor(private totalCount: number, private chunkSize: number) {
     this.items = [];
     const rounds = Math.ceil(totalCount / chunkSize);
     for (let i = 0; i < rounds; i++) {
       this.items.push({ offset: i * chunkSize, limit: chunkSize, chunkN: i });
     }
+
+    this.maxTotalItems = this.items.length;
   }
 
   next(): QueryMeta | undefined {
-    return this.items.pop();
+    return this.items.shift();
   }
 }
-
-export type QueryMeta = {
-  offset: number;
-  limit: number;
-  chunkN: number;
-};
