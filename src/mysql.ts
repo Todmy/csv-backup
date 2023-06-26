4fd6bae8a351c39bb199d5553ed3e788dd7874a6
Dmytro Tolok <tolok.d@gmail.com>
2023-06-26 11:41:45 +0200

    refactor(mysql): buitify the code

--- src/mysql.ts
+++ src/mysql.ts
@@ -1,17 +1,19 @@
 import mysql, { Pool } from 'mysql2';
 export { RowDataPacket, FieldPacket } from 'mysql2';
 
-const connection: Pool = mysql.createPool({
+const connectionConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
   waitForConnections: true,
-  connectionLimit: 2 * Number(process.env.N_THREADS),
-  maxIdle: 2 * Number(process.env.N_THREADS),
+  connectionLimit: Number(process.env.N_THREADS),
+  maxIdle: Number(process.env.N_THREADS),
   idleTimeout: 60000,
   enableKeepAlive: true
-});
+};
+
+const connection: Pool = mysql.createPool(connectionConfig);
 
 export default connection.promise();
